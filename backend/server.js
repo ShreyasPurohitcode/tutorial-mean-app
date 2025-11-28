119fb9c16d8386204911af045e6252fb51f41aa4
Shreyas Purohit <shreyaspurohit@Shreyas-MacBook-Air.local>
2025-11-28 17:38:18 +0530

    Fixing mongodb

--- backend/server.js
+++ backend/server.js
@@ -11,27 +11,27 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Test application." });
+    res.json({ message: "Welcome to Test application." });
 });
 
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
